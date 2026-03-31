7feaf7f931a5eaa75106a1da1debc81c08d10683
dosttiz <tfkpcalc@gmail.com>
2026-03-31 16:08:17 +0300

    Create main.h

---
+++ lab06/main.h
@@ -0,0 +1,46 @@
+/**
+  ******************************************************************************
+  * @file    Templates/BootCM4_CM7/CM7/Inc/main.h
+  * @author  MCD Application Team
+  * @brief   Header for main.c module for Cortex-M7.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2018 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+  
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+#include "stm32h7xx_nucleo.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Definition for TIMx clock resources */
+#define TIMx                           TIM2
+#define TIMx_CLK_ENABLE()              __HAL_RCC_TIM2_CLK_ENABLE()
+
+#define TIM3_CLK_ENABLE()              __HAL_RCC_TIM3_CLK_ENABLE()
+
+/* Definition for TIMx Channel Pins (LD1) */
+#define TIMx_GPIO_CLK_ENABLE()         __HAL_RCC_GPIOA_CLK_ENABLE()
+#define TIMx_GPIO_PORT                 GPIOA
+#define TIMx_GPIO_PIN                  GPIO_PIN_5
+#define TIMx_GPIO_AF                   GPIO_AF1_TIM2
+
+
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+#endif /* __MAIN_H */
+
