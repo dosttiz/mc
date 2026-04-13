b5dc45ff0274831dbe756c2f2c5a78b01e182a52
Versadinary <170194434+versadinary@users.noreply.github.com>
2026-04-13 05:58:41 +0300

    443242

---
+++ adc_dac_lab/example/Lab_09_ADC/Lab_09_ADC/CM7/Inc/main.h
@@ -0,0 +1,48 @@
+/**
+  ******************************************************************************
+  * @file    Templates/BootCM4_CM7/CM7/Inc/main.h
+  * @author  MCD Application Team
+  * @brief   Header for main.c module for Cortex-M7.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2018 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+  
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+#include "stm32h7xx_nucleo.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Definition for ADCx clock resources */
+#define ADCx                            ADC1
+#define ADCx_CLK_ENABLE()               __HAL_RCC_ADC12_CLK_ENABLE()
+#define ADCx_CHANNEL_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOF_CLK_ENABLE()
+
+#define ADCx_FORCE_RESET()              __HAL_RCC_ADC12_FORCE_RESET()
+#define ADCx_RELEASE_RESET()            __HAL_RCC_ADC12_RELEASE_RESET()
+
+/* Definition for ADCx Channel Pin */
+#define ADCx_CHANNEL_PIN                GPIO_PIN_11
+#define ADCx_CHANNEL_GPIO_PORT          GPIOF
+
+/* Definition for ADCx's Channel */
+#define ADCx_CHANNEL                    ADC_CHANNEL_2
+
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+#endif /* __MAIN_H */
+
